09293a1a7bc434781878b6614f5fd9fc23bc8213
Sumit Bhardwaj <40798467+nukeworm@users.noreply.github.com>
2019-04-07 14:23:17 +0530

    Create repeatedString.c

---
+++ repeatedString.c
@@ -0,0 +1,80 @@
+#include <assert.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* readline();
+
+// My Code Here
+long repeatedString(char* s, long n) {
+    long int a=0,oc,toc,extr,exa=0;
+    for(int i=0;i<strlen(s);i++)
+        if(s[i]=='a')
+            a++;
+    oc=n/strlen(s);
+    extr=n%strlen(s);
+    for(int i=0;i<extr;i++)
+        if(s[i]=='a')
+            exa++;
+    toc=(a*oc)+exa;
+    return toc;
+}
+
+//Pre-Written Code
+int main()
+{
+    FILE* fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+    char* s = readline();
+
+    char* n_endptr;
+    char* n_str = readline();
+    long n = strtol(n_str, &n_endptr, 10);
+
+    if (n_endptr == n_str || *n_endptr != '\0') { exit(EXIT_FAILURE); }
+
+    long result = repeatedString(s, n);
+
+    fprintf(fptr, "%ld\n", result);
+
+    fclose(fptr);
+
+    return 0;
+}
+
+char* readline() {
+    size_t alloc_length = 1024;
+    size_t data_length = 0;
+    char* data = malloc(alloc_length);
+
+    while (true) {
+        char* cursor = data + data_length;
+        char* line = fgets(cursor, alloc_length - data_length, stdin);
+
+        if (!line) { break; }
+
+        data_length += strlen(cursor);
+
+        if (data_length < alloc_length - 1 || data[data_length - 1] == '\n') { break; }
+
+        size_t new_length = alloc_length << 1;
+        data = realloc(data, new_length);
+
+        if (!data) { break; }
+
+        alloc_length = new_length;
+    }
+
+    if (data[data_length - 1] == '\n') {
+        data[data_length - 1] = '\0';
+    }
+
+    data = realloc(data, data_length);
+
+    return data;
+}
